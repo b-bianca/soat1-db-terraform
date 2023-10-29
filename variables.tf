db703318d102b5b4e9caf08d1eae9bb3cead7511
b-bianca <bianca.cms@outlook.com.br>
2023-10-29 15:30:54 -0300

    feat: database deployment

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "access_key" {
+    description = "Access key to AWS console"
+}
+variable "secret_key" {
+    description = "Secret key to AWS console"
+}
+variable "region" {
+    description = "AWS region"
+}
+variable "db_name" {
+    description = "Database name"
+}
+variable "db_password" {
+    description = "Database root password"
+}
+variable "db_user" {
+    description = "Database root user"
+}
