53c471cfbb5623d5aa247d7d0748d13d4c17fd4a
b-bianca <bianca.cms@outlook.com.br>
2023-11-06 05:26:49 -0300

    remove migration

--- main.tf
+++ main.tf
@@ -50,14 +50,3 @@ output "restaurant_database_address" {
   description = "Restaurant database address"
   value       = aws_db_instance.db_instance.address
 }
-
-# data "local_file" "sql_script" {
-#   filename = "${path.module}/migrations/sql/01_create_database.up.sql"
-# }
-
-# resource "null_resource" "db_setup" {
-#   depends_on = [aws_db_instance.db_instance, aws_security_group.rds_sg]
-#   provisioner "local-exec" {
-#     command = "mysql --host=${aws_db_instance.db_instance.address} --port=${var.DB_PORT} --user=${var.DB_USER} --password=${var.DB_PASSWORD} < ${data.local_file.sql_script.content}"
-#   }
-# }
